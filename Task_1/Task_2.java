cd17bc2660b4b917ae436f298a575d07d357f0d3
Fedor Shmurygin <73535847+Sogainame@users.noreply.github.com>
2024-02-09 22:01:02 +0600

    Task_2.java

--- Task_1/Task_2.java
+++ Task_1/Task_2.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class StringComparator {
+    public static void main(String[] args) {
+        // сканер объект для ввода с консоли
+        Scanner scanner = new Scanner(System.in);
+
+        // ввод первой строки
+        System.out.print("Enter the first string: ");
+        String stringA = scanner.nextLine();
+
+        // ввод второй строки
+        System.out.print("Enter the second string: ");
+        String stringB = scanner.nextLine();
+
+        // сравнить две строки
+        if (stringA.equals(stringB)) {
+            System.out.println("The strings are identical.");
+        } else {
+            System.out.println("The strings are not identical.");
+        }
+    }
+}
