6aa5cb0610dd577035a1dd52321011cea5091021
Fedor Shmurygin <73535847+Sogainame@users.noreply.github.com>
2024-02-09 22:03:43 +0600

    Task_3.java

--- Task_3.java
+++ Task_3.java
@@ -1,1 +1,11 @@
-
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Even numbers in the array are:");
+        for (int i : arr) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
